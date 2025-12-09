aab225082f69d4abee2ecbd939628231b39f792b
Pedro-E00 <pedrogs2y@gmail.com>
2025-12-08 22:19:08 -0300

    últimos detalhes
    Co-authored-by: Kaysheen <Puccinella@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Projeto-Final
-Projeto final do trabalho de web do 2° Ano Informática.
+Projeto final do trabalho de web do 2° Ano Informática
+
+
+O projeto consiste em um sistema de vendas de jogos, no qual há o cadastro de usuário, jogos e pedidos.
+Existem dois tipos de usuário, sendo o admin, que é pré-cadastrado no código, e 
 
 Nomes: Kaysheen, Miguel Cesar, Pedro Elias e Lucas Andrade.
 Entregar no dia 9 de novembro de 2025.
--- src/models/relacoes.cjs
+++ src/models/relacoes.cjs
@@ -4,15 +4,13 @@ const { Usuario } = require("./usuario.cjs");
 const { itemPedido } = require("./itemPedido.cjs");
 
 function relacionarModels() {
-    // Usuario 1:N Pedido
+
     Usuario.hasMany(Pedido, { foreignKey: 'comprador_id' });
     Pedido.belongsTo(Usuario, { foreignKey: 'comprador_id' });
 
-    // Pedido 1:N PedidoItem
     Pedido.hasMany(itemPedido, { foreignKey: 'pedido_id' });
     itemPedido.belongsTo(Pedido, { foreignKey: 'pedido_id' });
 
-    // Jogo 1:N PedidoItem
     Jogo.hasMany(itemPedido, { foreignKey: 'jogo_id' });
     itemPedido.belongsTo(Jogo, { foreignKey: 'jogo_id' });
 };
